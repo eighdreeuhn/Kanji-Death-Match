15cccdad078b866fd252cf79e26449a5cb6ecb41
Adrian Gioia <afgioia@gmail.com>
2022-05-15 16:20:43 -0700

    Project-2 pre-work set-up. Created GitHub repo, installed react router & DOM, laid routes foundation.

---
+++ kanji-death-match/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
