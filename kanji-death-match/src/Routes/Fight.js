ec0fd622716affa1f3472b4db5c6efe60b1e2d06
Adrian Gioia <afgioia@gmail.com>
2022-05-18 12:29:32 -0700

    Adjusted the logic for the fight sequence so that HP is updated correctly. Moving on to handle edge cases and rendering the kanji.

--- kanji-death-match/src/Routes/Fight.js
+++ kanji-death-match/src/Routes/Fight.js
@@ -1,43 +1,69 @@
-import { useState } from 'react';
-import Header from '../Components/Header';
+import { useState, useEffect } from 'react';
 
 const Fight = (props) => {
-
+console.log(props)
     //Assign battle stats to each kanji based on data
+
     let p1 = props.fighters[0];
     let p2 = props.fighters[1];
-    p1.hp = (p1.kanji.strokes.count * p1.radical.strokes) ** 2;
-    p2.hp = (p2.kanji.strokes.count * p2.radical.strokes) ** 2;
-    p1.attackPower = p1.hp / 100;
-    p2.attackPower = p2.hp / 100;
-    let gameClock;
-    let round = 1;
-    console.log(p1.attackPower, p2.attackPower);
+    const [player1Hp, setPlayer1Hp] = useState();
+    const [player2Hp, setPlayer2Hp] = useState()
+    // const [fight, setFight] = useState([]);
 
+    p1.attackPower = p1.kanji.strokes.count * 5;
+    p2.attackPower = p2.kanji.strokes.count * 5;
+    let gameClock;
+    console.log(p1.attackPower, p2.attackPower);
+    
     const handleBattle = () => {
+        console.log("here");
+        let round = 1
+        let rngAttack;
+        let p1HpCopy;
+        let p2HpCopy;
         gameClock = setInterval(() => {
-            switch (round % 2 === 0) {
-                case true :
-                    console.log("p1");
-                    break;
-                case false :
-                    console.log("p2");
-                    break;
+            if (round === 1) {
+                console.log(p1.kanji.strokes.count * p1.radical.strokes + 2000)
+                console.log(p2.kanji.strokes.count * p2.radical.strokes + 2000)
+                setPlayer1Hp(p1.kanji.strokes.count * p1.radical.strokes + 2000);
+                setPlayer2Hp(p2.kanji.strokes.count * p2.radical.strokes + 2000);
+                p1HpCopy = player1Hp;
+                p2HpCopy = player2Hp;
             }
-            round++;
-            console.log(round)
-        }, 1000)
+            console.log("HP status:", p1HpCopy, p2HpCopy)
+            if (player1Hp > 0 && player2Hp > 0) {
+                switch (round % 2 === 0) {
+                    case true :
+                        rngAttack = Math.floor(Math.random() * p1.attackPower);
+                        p2HpCopy -= rngAttack;
+                        setPlayer2Hp(p2HpCopy);
+                        console.log(`p1 attacks for: ${rngAttack} damage!`, player2Hp);
+                        break;
+                    case false :
+                        rngAttack = Math.floor(Math.random() * p2.attackPower);
+                        p1HpCopy -= rngAttack;
+                        setPlayer1Hp(p1HpCopy);
+                        console.log(`p2 attacks for: ${rngAttack} damage!`, player1Hp);
+                        break;
+                }
+                round++;
+                console.log(round)
+            } else {
+                clearInterval(gameClock);
+                // player1Hp <= 0 ? setPlayer1Hp(0) : setPlayer2Hp(0)
+            }
+        }, 3000)
     }
 
-    setTimeout(handleBattle, 3000)
+    // setTimeout(handleBattle, 3000)
      
     return (
         <div className="Dojo">
-            <headeer className="battleStats">
-                <div className="player-1-hp"><h1>{p1.hp}</h1></div>
-                <div className="logo"></div>
-                <div className="player-2-hp"><h1>{p2.hp}</h1></div>
-            </headeer>
+            <header className="battleStats">
+                <div className="player-1-hp"><h1>{player1Hp}</h1></div>
+                <button className="start" onClick={() => setTimeout(handleBattle, 1000)}>FIGHT!!!</button>
+                <div className="player-2-hp"><h1>{player2Hp}</h1></div>
+            </header>
             <section className="battle-stats">
                 <div></div>
                 <div></div>
