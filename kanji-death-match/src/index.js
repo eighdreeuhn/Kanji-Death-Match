f1e4eec4820e7c4f6c94a0ed5a651d105f414fce
Adrian Gioia <afgioia@gmail.com>
2022-05-19 21:11:51 -0700

    Deploy debug. Trying HashRouter

--- kanji-death-match/src/index.js
+++ kanji-death-match/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App';
